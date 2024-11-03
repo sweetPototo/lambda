7ea21857cede487b153c656510d7402b7c28425d
sweetpototo <sweetpototo11@gmail.com>
2024-11-04 08:08:53 +0900

    fix main class

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ public class Main {
         Lambda1 lambda1 = new Lambda1();
         Lambda2 calculator = new Lambda2();
 
+        System.out.println("[Lambda1 class 실행]");
         lambda1.lambda1_run();
         System.out.println();
+
+        System.out.println("[Lambda2 class 실행]");
         calculator.calculator_run();
     }
 }
